7e64bd82bc6fa64f67837dc19d14ee4d6f63e78a
gaurav1207 <32225208+gaurav1207@users.noreply.github.com>
2018-01-28 04:07:35 +0530

    Add files via upload

---
+++ spot it 1/popup.js
@@ -0,0 +1,8 @@
+function loadUrls()
+{
+}
+function saveUrls(){
+}
+document.getElementById('button').addEventListener('click',loadUrls);
+document.getElementById('button').addEventListener('click',saveUrls);
+windows.open('https://goo.gl/forms/1zY36wuoadsRyfVK2','_newtab');
